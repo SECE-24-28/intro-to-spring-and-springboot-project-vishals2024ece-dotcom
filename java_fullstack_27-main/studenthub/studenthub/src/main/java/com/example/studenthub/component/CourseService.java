9b46ff7a4d821a8598a6f9a1ee3d91121396aef9
vishals2024ece-dotcom <vishal.s2024ece@sece.ac.in>
2026-06-07 13:27:42 +0530

    Add files via upload

---
+++ java_fullstack_27-main/studenthub/studenthub/src/main/java/com/example/studenthub/component/CourseService.java
@@ -0,0 +1,11 @@
+package com.example.studenthub.component;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+    public String getCourse() {
+        return "Java Spring Boot";
+    }
+}
